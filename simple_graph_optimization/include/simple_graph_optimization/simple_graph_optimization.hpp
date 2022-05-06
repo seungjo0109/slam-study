783d863c17b0c039dbb63e1889bd7ce126863cd0
seungjo0109 <seungjo0109@gmail.com>
2022-05-06 13:35:54 +0900

    Refactoring SimpleGraphOptimization class

--- simple_graph_optimization/include/simple_graph_optimization/simple_graph_optimization.hpp
+++ simple_graph_optimization/include/simple_graph_optimization/simple_graph_optimization.hpp
@@ -13,49 +13,67 @@ class SimpleGraphOptimization : public rclcpp::Node
 {
 public:
     SimpleGraphOptimization() : Node("simple_graph_optimization"){
-        std::cout << "SimpleGraphOptimization" << std::endl;
-
+        
         /* Publisher & Subscriber initialization */
+        InitRosPublisher();
+
+        /* Node initialization */
+        pose_graph_optimizer_   = std::make_unique<PoseGraphOptimizer>();
+        sim_model_              = std::make_unique<SimModel>(15);
+        visualizer_             = std::make_unique<SimVisualizer>();
+
+        /* Simulation Initialization */ 
+        sim_model_->GenerateTrueState();                            // Generte ground truth
+        sim_model_->GenerateSimState();                             // Generate noised state
+        sim_model_->AddVertex();                                    // Add vetex into optimizer
+        sim_model_->AddEdge();                                      // Add edge into optimizer
+        sim_model_->GetOptimizer()->initializeOptimization();       // Initialize optimizer
+        sim_model_->GetOptimizer()->setVerbose(true);
+        // sim_model_->Optimize(20);
+
+        auto NodeRunner = [this]()-> void{
+            /* Optimize graph once per loop iteration */
+            sim_model_->OptimizeOnce();
+
+            /* Set visualization messages */
+            SetVisualizationMsg();
+            
+            /* Publish ROS messages */
+            PublishMsg();
+        };
+
+        timer_ = this->create_wall_timer(std::chrono::milliseconds{100}, NodeRunner);  
+    }
+
+private:
+    /* Publisher & Subscriber initialization */
+    void InitRosPublisher(){
         true_node_pub_          = this->create_publisher<visualization_msgs::msg::MarkerArray>("true_node", 10);
         true_edge_pub_          = this->create_publisher<visualization_msgs::msg::MarkerArray>("true_edge", 10);
         sim_node_pub_           = this->create_publisher<visualization_msgs::msg::MarkerArray>("sim_node", 10);
         sim_edge_pub_           = this->create_publisher<visualization_msgs::msg::MarkerArray>("sim_edge", 10);
         optimized_node_pub_     = this->create_publisher<visualization_msgs::msg::MarkerArray>("optimized_node", 10);
         optimized_edge_pub_     = this->create_publisher<visualization_msgs::msg::MarkerArray>("optimized_edge", 10);
+    }
 
-        /* Node initialization */
-        pose_graph_optimizer_   = std::make_unique<PoseGraphOptimizer>();
-        sim_model_              = std::make_unique<SimModel>(15);
-        visualizer_             = std::make_unique<SimVisualizer>();
-        sim_model_->GetOptimizer()->clear();
-
-        /* Run */ //TODO: move to NodeRunner    
-        sim_model_->GenerateTrueState();
-        sim_model_->GenerateSimState();
-        sim_model_->AddVertex();
-        sim_model_->AddEdge();
-
-        sim_model_->GetOptimizer()->initializeOptimization();
-        sim_model_->GetOptimizer()->setVerbose(true);
-        sim_model_->Optimize(20);
-        
+    /* Set visualization messages */
+    void SetVisualizationMsg(){
         visualizer_->SetVisualizationMsg(SimVisualizer::DataType::TrueNode, *sim_model_);
         visualizer_->SetVisualizationMsg(SimVisualizer::DataType::TrueEdge, *sim_model_);
         visualizer_->SetVisualizationMsg(SimVisualizer::DataType::SimNode, *sim_model_);
         visualizer_->SetVisualizationMsg(SimVisualizer::DataType::SimEdge, *sim_model_);
         visualizer_->SetVisualizationMsg(SimVisualizer::DataType::OptimizedNode, *sim_model_);
         visualizer_->SetVisualizationMsg(SimVisualizer::DataType::OptimizedEdge, *sim_model_);
+    }
 
-        auto NodeRunner = [this]()-> void{
-            true_node_pub_->publish(visualizer_->GetVisualizationMsg(SimVisualizer::DataType::TrueNode));
-            true_edge_pub_->publish(visualizer_->GetVisualizationMsg(SimVisualizer::DataType::TrueEdge));
-            sim_node_pub_->publish(visualizer_->GetVisualizationMsg(SimVisualizer::DataType::SimNode));
-            sim_edge_pub_->publish(visualizer_->GetVisualizationMsg(SimVisualizer::DataType::SimEdge));
-            optimized_node_pub_->publish(visualizer_->GetVisualizationMsg(SimVisualizer::DataType::OptimizedNode));
-            optimized_edge_pub_->publish(visualizer_->GetVisualizationMsg(SimVisualizer::DataType::OptimizedEdge));
-        };
-
-        timer_ = this->create_wall_timer(std::chrono::milliseconds{1000}, NodeRunner);  
+    /* Publish ROS messages */
+    void PublishMsg(){
+        true_node_pub_->publish(visualizer_->GetVisualizationMsg(SimVisualizer::DataType::TrueNode));
+        true_edge_pub_->publish(visualizer_->GetVisualizationMsg(SimVisualizer::DataType::TrueEdge));
+        sim_node_pub_->publish(visualizer_->GetVisualizationMsg(SimVisualizer::DataType::SimNode));
+        sim_edge_pub_->publish(visualizer_->GetVisualizationMsg(SimVisualizer::DataType::SimEdge));
+        optimized_node_pub_->publish(visualizer_->GetVisualizationMsg(SimVisualizer::DataType::OptimizedNode));
+        optimized_edge_pub_->publish(visualizer_->GetVisualizationMsg(SimVisualizer::DataType::OptimizedEdge));
     }
 
 private:
