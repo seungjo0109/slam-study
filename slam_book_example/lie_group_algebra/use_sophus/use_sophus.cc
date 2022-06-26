979ccd8d9eeaf9e9cf0c248bef9f960ae8f588bf
seungjo0109 <seungjo0109@gmail.com>
2022-06-26 22:26:22 +0900

    Error fix(use_sophus example)

--- slam_book_example/lie_group_algebra/use_sophus/use_sophus.cc
+++ slam_book_example/lie_group_algebra/use_sophus/use_sophus.cc
@@ -11,11 +11,11 @@ int main()
 {
     // Rotation matrix with 90 degrees along Z axis
     Eigen::Matrix3d R = Eigen::AngleAxisd(M_PI/2, Eigen::Vector3d(0,0,1)).toRotationMatrix();
-    Eigen::Quaternion q(R);
+    Eigen::Quaterniond q(R);
 
     // Sophus::SO3d can be constructed from rotation matrix
-    Sophus::So3d SO3_R(R);
-    Sophus::So3d SO3_q(q);
+    Sophus::SO3d SO3_R(R);
+    Sophus::SO3d SO3_q(q);
 
     std::cout << "SO(3) from matrix: \n" << SO3_R.matrix() << std::endl;
     std::cout << "SO(3) from quaternion: \n" << SO3_q.matrix() << std::endl;
@@ -23,18 +23,18 @@ int main()
 
     // Use logarithmic map to get the Lie algebra
     Eigen::Vector3d so3 = SO3_R.log();
-    std::cout << "so3 = " << so3.transpose() << endl;
+    std::cout << "so3 = " << so3.transpose() << std::endl;
 
     // hat is from vector to skew-symmetric matrix
     std::cout << "so3 hat=\n" << Sophus::SO3d::hat(so3) << std::endl;
     
     // inversely from matrix to vector
-    std::cout << "so3 hat vee= " << Sohpus::SO3d::vee(Sophus::SO3d::hat(so3)).transpose() << std::endl;
+    std::cout << "so3 hat vee= " << Sophus::SO3d::vee(Sophus::SO3d::hat(so3)).transpose() << std::endl;
 
     // Update by perturbation model
     Eigen::Vector3d update_so3(1e-4,0,0);   // this is a small update
-    Sohpus::SO3d SO3_updated = Sophus::SO3d::exp(update_so3) * SO3_R;
-    std::cout << "SO3 updated = \n" << SO3_udpated.matrix() << std::endl;
+    Sophus::SO3d SO3_updated = Sophus::SO3d::exp(update_so3) * SO3_R;
+    std::cout << "SO3 updated = \n" << SO3_updated.matrix() << std::endl;
 
     std::cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << std::endl;
     // Similar for SE(3)
@@ -56,7 +56,7 @@ int main()
     Vector6d update_se3;
     update_se3.setZero();
     update_se3(0,0) = 1e-4d;
-    Sohpus::SE3d SE3_updated = Sophus::SE3d::exp(update_se3) * SE3_Rt;
+    Sophus::SE3d SE3_updated = Sophus::SE3d::exp(update_se3) * SE3_Rt;
     std::cout << "SE3 updated = " << std::endl << SE3_updated.matrix() << std::endl;
 
     return 0;
